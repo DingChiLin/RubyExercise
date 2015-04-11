4887c1ebed770300fa886f293f8c4c42426f724c
Arthur Lin <Sealion@Arthurteki-MacBook-Pro.local>
2015-04-11 14:49:13 +0800

    finish QuickSort

--- Algorithms/SortAlgorithms/QuickSort.rb
+++ Algorithms/SortAlgorithms/QuickSort.rb
@@ -8,13 +8,15 @@ class QuickSort
 
   end
 
+  private
+
   def sortByRange!(arr,l,r)
 
-    #1. find pivit point
-    p_index,p_value = findPivot(arr,l,r)
+    #1. find pivit point and make it to be the first element
+    p_value = findPivot!(arr,l,r)
 
     #2. change arr element order according to pivit point
-    p_d = reorder!(arr,p_index,p_value,l,r)
+    p_d = reorder!(arr,p_value,l,r)
 
     #3. resursive call
     sortByRange!(arr,l,p_d-1) if p_d-1 > l
@@ -22,16 +24,16 @@ class QuickSort
 
   end
 
-  def findPivot(arr,l,r)
-    return l, arr[l]
+  def findPivot!(arr,l,r)
+    exchange!(arr,l,l+rand(r-l+1))
+    arr[l]
   end
 
-  def reorder!(arr,p_index,p_value,l,r)
+  def reorder!(arr,p_value,l,r)
 
     p_d = l+1 # p_division
     for i in l+1..r
       if arr[i] > p_value
-
         exchange!(arr,i,p_d)
         p_d += 1
       end
@@ -50,8 +52,8 @@ class QuickSort
 
 end
 
-qs = QuickSort.new
-arr = [3,8,2,5,1,4,7,6]
-arr = qs.sort(arr)
-puts "reorder" + arr.inspect
+#qs = QuickSort.new
+#arr = [3,8,2,5,1,4,7,6]
+#arr = qs.sort(arr)
+#puts "reorder" + arr.inspect
 
